ff22882b21379824de3d85a8fc9567ee746afb8b
DJ.Zhang <dengji@evg.co>
2024-06-26 19:06:14 +0800

    modified:   src/viem.ts

--- src/viem.ts
+++ src/viem.ts
@@ -172,6 +172,45 @@ export default class EthereumRpc {
       message: originalMessage,
     });
 
+    const domain = {
+      name: "Ether Mail",
+      version: "1",
+      chainId: 97,
+      verifyingContract: address[0],
+    };
+
+    // The named list of all type definitions
+    const types = {
+      Person: [
+        { name: "name", type: "string" },
+        { name: "wallet", type: "address" },
+      ],
+      Mail: [
+        { name: "from", type: "Person" },
+        { name: "to", type: "Person" },
+        { name: "contents", type: "string" },
+      ],
+    };
+    const [account] = await walletClient.getAddresses();
+    const signature = await walletClient.signTypedData({
+      account,
+      domain,
+      types,
+      primaryType: "Mail",
+      message: {
+        from: {
+          name: "Cow",
+          wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+        },
+        to: {
+          name: "Bob",
+          wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+        },
+        contents: "Hello, Bob!",
+      },
+    });
+    console.log(signature);
+
     console.log(hash);
 
     return hash.toString();
